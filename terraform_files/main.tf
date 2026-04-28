b38d980a813df7c3fdd24ae2b6c949138eff3e72
Theo-Panella <panellatheo@gmail.com>
2026-04-28 07:38:51 -0300

    Update: AMI configurated using vars from variables.tf (Instance_configurations)

--- terraform_files/main.tf
+++ terraform_files/main.tf
@@ -3,6 +3,11 @@ provider "aws" {
   profile     = "default"
 }
 
+data "aws_ami" "ubuntu"{
+  most_recent = var.instance_configurations.most_recent
+  owners = var.instance_configurations.ami_code
+}
+
 # --- Arquitetura de Rede --- 
 resource "aws_vpc" "VPC1" {
   cidr_block = "192.168.0.0/28"
