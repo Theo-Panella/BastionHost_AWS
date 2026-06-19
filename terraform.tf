6bd609acb6e18bbee6674434df684983943de3fc
Theo-panella <panellatheo@gmail.com>
2026-06-19 13:40:25 -0300

    Revert: remove S3 backend configuration

--- terraform.tf
+++ terraform.tf
@@ -6,12 +6,4 @@ terraform {
     }
   }
   required_version = ">= 1.2"
-  
-  backend "s3" {
-    bucket         = "meu-bucket-tfstate"
-    key            = "bastionhost/terraform.tfstate"
-    region         = "us-west-2"
-    dynamodb_table = "terraform-lock"
-  }
-
-}
+} 
