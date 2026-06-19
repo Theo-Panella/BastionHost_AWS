f94794bfc977b20fbb0e5a36788fcde60e59cb8d
Theo-panella <panellatheo@gmail.com>
2026-06-19 13:40:03 -0300

    Update: configure S3 backend for terraform state

--- terraform.tf
+++ terraform.tf
@@ -5,6 +5,13 @@ terraform {
       version = "~> 5.92"
     }
   }
-
   required_version = ">= 1.2"
+  
+  backend "s3" {
+    bucket         = "meu-bucket-tfstate"
+    key            = "bastionhost/terraform.tfstate"
+    region         = "us-west-2"
+    dynamodb_table = "terraform-lock"
+  }
+
 }
